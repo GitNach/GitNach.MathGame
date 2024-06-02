2414141ce1aba8a186b3b908a89e85f35ca25d42
GitNach <137569683+GitNach@users.noreply.github.com>
2024-06-01 23:36:50 -0300

    Revert "Added Division game functionality"
    This reverts commit efe5bf2a031f25de9ae27918dec3cbfe76611983.

--- WinFormsMath1/Menu.cs
+++ WinFormsMath1/Menu.cs
@@ -57,13 +57,5 @@ namespace WinFormsMath1
         {
 
         }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            DivisionForm divitionForm = new DivisionForm();
-            this.Hide();    
-            divitionForm.ShowDialog();  
-            this.Show();
-        }
     }
 }
